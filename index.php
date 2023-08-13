3a4b4b3cdd293e7eaa7fc9d6a73d0daaf9f61020
sakuwell <taikisakurai0212@gmail.com>
2023-08-13 19:46:00 +0900

    検索メソッド追加

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-// スタッフ新規登録
+// スタッフデータ新規登録
 function writeStaffCsv($id, $name, $sex, $position){
     $csvFilePath = 'csv/staff.csv';
     $file = fopen($csvFilePath, 'r');
@@ -22,10 +22,11 @@ function writeStaffCsv($id, $name, $sex, $position){
         fclose($file);
     } else {
         echo "ファイルを開けませんでした。";
+        return false;
     }
 }
 
-// スタッフ一覧読み込み
+// スタッフデータ一覧読み込み
 function allStaffCsv(){
     $csvFilePath = 'csv/staff.csv';
     $file = fopen($csvFilePath, 'r');
@@ -39,12 +40,53 @@ function allStaffCsv(){
         return $staffData;
     } else {
         echo "ファイルを開けませんでした。";
+        return false;
     }
 }
 
-$staffData[] = allStaffCsv();
-foreach($staffData as $row){
-    foreach($row as $data){
-        echo("ID: ". $data[0] . "\t" . "名前: " . $data[1] . "\t" . "性別: " . $data[2] . "\t" . "階級: " . $data[3]."\n");
+//1件のデータ呼び出し
+function targetStaffCsv($id){
+    // CSVファイルのパス
+    $csvFilePath = 'csv/staff.csv';
+
+    // ファイルを読み込む
+    $file = fopen($csvFilePath, 'r');
+    if ($file) {
+        while (($row = fgetcsv($file)) !== false) {
+            if ($row[0] == $id) {
+                // 目的の行のデータを取得
+                $targetStaffData = $row;
+                break;
+            }
+        }
+        fclose($file);
+
+        if (isset($targetStaffData)) {
+            // $targetRowDataに目的の行のデータが格納されています
+            return $targetStaffData;
+        } else {
+            echo "指定した行が見つかりませんでした。";
+            return false;
+        }
+    } else {
+        echo "ファイルを開けませんでした。";
+        return false;
     }
+
 }
+
+
+
+
+// $s[] = targetStaffCsv(2);
+// foreach($s as $data){
+//     echo ($data[0].$data[1].$data[2].$data[3]);
+// }
+
+// $staffData[] = allStaffCsv();
+// foreach($staffData as $row){
+//     foreach($row as $data){
+//         echo("ID: ". $data[0] . "\t" . "名前: " . $data[1] . "\t" . "性別: " . $data[2] . "\t" . "階級: " . $data[3]."\n");
+//     }
+// }
+
